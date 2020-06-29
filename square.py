72e25dc61cdda669b5d421495a142dfa7e117c6d
vijayboltreddy007 <vbreddy804@gmail.com>
2020-06-29 11:55:21 +0530

    Create square.py

---
+++ square.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 11:50:44 2020
+
+@author: PienApple
+"""
+
+import turtle
+bolt = turtle.Turtle()
+bolt.color("black")
+for side in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]:
+    bolt.forward(100)
+    bolt.right(90)
