ba075d6882d51a012a98730dfa84d8f91fc008ac
vijayboltreddy007 <vbreddy804@gmail.com>
2020-06-29 15:27:25 +0530

    Create complete.py

---
+++ complete.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 29 15:26:48 2020
+
+@author: PienApple
+"""
+
+import turtle
+amy = turtle.Turtle()
+
+# Make the width thicker so that the line will be easier to see
+amy.width(5)
+
+# Move back without drawing anything
+amy.penup()
+amy.back(140)
+amy.pendown()
+
+# Draw a red line
+amy.color("red")
+amy.forward(50)
+
+# Move forward without drawing anything
+amy.penup()
+amy.forward(50)
+amy.pendown()
+
+# Draw an orange line
+amy.color("orange")
+amy.forward(50)
+
+# Move forward without drawing anything
+amy.penup()
+amy.forward(50)
+amy.pendown()
+
+# Draw a yellow line
+amy.color("yellow")
+amy.forward(50)
