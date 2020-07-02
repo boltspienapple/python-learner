6daf5cafaff151d809b45d2f4faf6e9c28ccd95b
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-02 22:30:43 +0530

    Create mood.py

---
+++ Functions/mood.py
@@ -0,0 +1,16 @@
+mood = "happy"
+
+import turtle
+riley = turtle.Turtle()
+riley.width(5)
+
+if mood == "happy":
+    riley.color("yellow")
+elif mood == "sad":
+    riley.color("blue")
+else:
+    riley.color("gray")
+
+for side in range(5):
+    riley.forward(100)
+    riley.right(144)
