7458e3419cda7fc06f59fd111b897e70cd5c0674
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-03 21:27:02 +0530

    updated

---
+++ count.py
@@ -0,0 +1,5 @@
+count = 0
+for ch in "bonobos":
+    if ch == "o":
+        count += 1
+print(count)
---
+++ loop.py
@@ -0,0 +1,10 @@
+message = input("What do you have to say, hm?\n")
+
+for ch in message:
+    # Add your code here.
+    
+    print(ch)
+    if ch == "!":
+        print("I'm excited too!!!")
+    if ch == "?":
+        print("i am nothing")
