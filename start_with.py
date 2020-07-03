a3201c4211b55a31604b9c5c300fde2dab05cd84
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-03 22:25:48 +0530

    Create start_with.py

---
+++ start_with.py
@@ -0,0 +1,11 @@
+# Write your function definition here
+def start_K(s):
+    if s[0] == "K":
+        return True
+    else:
+        return False
+# A function call like this should return True:
+print(start_K("Kelly"))
+
+# And one like this should return False:
+print(start_K("Abe"))
