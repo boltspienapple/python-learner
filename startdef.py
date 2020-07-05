cc2f9c56e5f0efc5a88bedc99f4d59fe221b08c8
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-05 12:46:39 +0530

    update

---
+++ startdef.py
@@ -0,0 +1,7 @@
+def possible_tag(word):
+    if word.startswith("<") and word.endswith(">"):
+        print(word, "could maybe be an HTML tag")
+    else:
+        print(word, "is definitely not an HTML tag (but might contain one)")
+
+possible_tag("<>")
