cb0b9a976502c0c28ca73c6ad1b54e943e3d7eda
vijayboltreddy007 <vbreddy804@gmail.com>
2020-07-06 22:03:17 +0530

    Create join.py

---
+++ join.py
@@ -0,0 +1,7 @@
+lines = ["Haiku frogs in snow",
+         "A limerick came from Nantucket",
+         "Tetrametric drum-beats thrumming, Hiawathianic rhythm."]
+
+def breakify(strings):
+    return "<br>".join(strings)
+print(breakify(lines))
